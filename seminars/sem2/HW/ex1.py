8fec9101ceb7b1d2e40e0bbbb9e78edddafb44d2
Like_Abbos <atammura.shin@gmail.com>
2022-09-27 23:06:53 +0300

    ex1 bug fix

--- seminars/sem2/HW/ex1.py
+++ seminars/sem2/HW/ex1.py
@@ -8,5 +8,17 @@ def DigitSum (chislo_list):
             chislo_list[i]=chislo_list[i]//10
     return sum
 
-chislo = list(map(int, input('Введите вещественное: ').split('.')))
-print(DigitSum(chislo))
+def DigitSumInt (chislo):
+    sum = 0
+    while chislo>0:
+        sum+=chislo%10
+        chislo=chislo//10
+    return sum
+
+user_inp = input('Введите вещественное: ')
+if ('.' or ',' in user_inp):
+    user_inp=user_inp.replace(',','.')
+    chislo = list(map(int, user_inp.split('.')))
+    print(DigitSum(chislo))
+else:
+    print(DigitSum(DigitSumInt(int(user_inp))))
