24e068c5081b2a76591c2a9b3353dc61cbb1dd9c
Like_Abbos <atammura.shin@gmail.com>
2022-10-03 23:25:51 +0300

    hw ex4 over

---
+++ seminars/sem2/HW/ex4_1.py
@@ -0,0 +1,19 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число
+
+from pdb import line_prefix
+
+
+n = int(input('Введите число: '))
+numbers = list(range(-n,n+1))
+sum=0
+pos = []
+file='C:\\Users\\МSI\\Desktop\\Python for n00bz\\seminars\\sem2\\HW\\positions.txt'
+data = open(file,'r')
+for line in data:
+    pos.append(int(line))
+print (numbers, '\n', pos)
+for i in range(len(pos)):
+    index=pos[i]
+    sum+=numbers[index]
+print (sum)
