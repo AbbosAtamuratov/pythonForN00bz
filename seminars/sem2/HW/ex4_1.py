56cd31976b6975117826e30c07ed8c25a5c4bc3f
Like_Abbos <atammura.shin@gmail.com>
2022-10-03 23:28:05 +0300

    hw ex4 cosmetic fixes

--- seminars/sem2/HW/ex4_1.py
+++ seminars/sem2/HW/ex4_1.py
@@ -9,11 +9,11 @@ numbers = list(range(-n,n+1))
 sum=0
 pos = []
 file='C:\\Users\\МSI\\Desktop\\Python for n00bz\\seminars\\sem2\\HW\\positions.txt'
-data = open(file,'r')
-for line in data:
-    pos.append(int(line))
-print (numbers, '\n', pos)
-for i in range(len(pos)):
-    index=pos[i]
-    sum+=numbers[index]
-print (sum)
+with open(file,'r') as data:
+    for line in data:
+        pos.append(int(line))
+    print (numbers, '\n', pos)
+    for i in range(len(pos)):
+        index=pos[i]
+        sum+=numbers[index]
+    print (sum)
