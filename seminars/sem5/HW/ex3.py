ac69c1d2c160f8ac6122d35a62e6232ec15cf65f
Like_Abbos <atammura.shin@gmail.com>
2022-10-15 22:49:59 +0300

    hw6 ex1 redone

--- seminars/sem5/HW/ex3.py
+++ seminars/sem5/HW/ex3.py
@@ -1,15 +1,37 @@
 # Создайте программу для игры в ""Крестики-нолики""
+import random
 
-clear_board = [['[_]','[_]','[_]'], ['[_]','[_]','[_]'], ['[_]','[_]','[_]']]
+clear_board = [i for i in range (1,10)]
 tic = 'X'
 tac = 'O'
 
-m = int(input('Введите номер клетки, куда вы хотите сделать ход:'))
-numbered_board='[1][2][3]\n[4][5][6]\n[7][8][9]'
+def DrawBoard(b):
+    print('-' * 15)
+    k=1
+    for i in range(len(b)):
+        print(f'| {b[i]} |', end='')
+        if k%3==0:
+            print('\n'+'-'*15)
+        k+=1
 
-def PutItThere(t, m, b):
-    occupied = []
-    m=0
-    while (m<10 or m>0) and m not in occupied:
-        m = int(input('Введите номер клетки, куда вы хотите сделать ход:'))
-    
+# DrawBoard(clear_board)
+
+def RunGame(tic, tac, b):
+    play_board = b
+    player_doko= lambda x, a, b: a if x else b
+    coin_toss = bool(random.randint(0,2))
+    pos = int(input('В какую клетку ходим? '))
+    k=0
+    while k<10:
+        # while (not 0<pos<10) or (pos not in b):
+        #     print('Такой клетки нет или занята... Попробуйте ещё раз.')
+        #     pos = int(input('В какую клетку ходим? '))
+        for i in range(len(b)):
+            if pos == b[i]:
+                b[i] = player_doko(coin_toss, tic, tac)
+        DrawBoard(b)
+        coin_toss= not coin_toss
+        k+=1
+
+DrawBoard(clear_board)
+RunGame(tic, tac, clear_board)
