9212e9262e3c222e061cdce4631183221b59c968
Like_Abbos <atammura.shin@gmail.com>
2022-10-28 22:30:18 +0300

    hw7 debug session 1

--- seminars/sem7/HW/impex.py
+++ seminars/sem7/HW/impex.py
@@ -4,7 +4,7 @@ def import_phonebook(inp_list) -> list:
     with open('phonebook.txt', 'r') as file:
         item = {"name": '', "surname": '', "tel": ''}
         for i in file:
-            tmp = list(map(i.split('-')))
+            tmp = list(map(str, i.split('-')))
             item["name"] = tmp[0]
             item["surname"] = tmp[1]
             item["tel"] = tmp[2]
@@ -13,6 +13,6 @@ def import_phonebook(inp_list) -> list:
 
 
 def export_phonebook(inp_list):
-    with open('phonebook.txt', 'w') as file:
+    with open('phonebook.txt', 'w+') as file:
         for i in range(len(inp_list)):
-            file.append(f'{inp_list[i]["name"]} - {inp_list[i]["surname"]} - {inp_list[i]["tel"]}')
+            file.write(f'{inp_list[i]["name"]} - {inp_list[i]["surname"]} - {inp_list[i]["tel"]}\n')
