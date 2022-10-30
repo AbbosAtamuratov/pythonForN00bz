02859b2e9db799c95cdb7e52f7819a2cb3a4c502
Like_Abbos <atammura.shin@gmail.com>
2022-10-30 17:55:30 +0300

    hw 8 almost done

--- seminars/sem8/HW/impex.py
+++ seminars/sem8/HW/impex.py
@@ -1,1 +1,34 @@
 # импрота и экспорта модуль
+
+import json
+import csv
+def import_txt() -> list:
+    with open('database.txt', 'r+') as file:
+        items = []
+        for i in file:
+            item = {"name": '', "surname": '', "tel": '', "assignment": '', "wage": ''}
+            i=i.strip('\n')
+            tmp = list(map(str, i.split('-')))
+            item["name"] = tmp[0]
+            item["surname"] = tmp[1]
+            item["tel"] = tmp[2]
+            item["assignment"] = tmp[3]
+            item["wage"] = tmp[4]
+            items.append(item)
+    return items
+
+def export_txt(inp_list):
+    with open('database.txt', 'w+') as file:
+        for i in range(len(inp_list)):
+            file.write(f'{inp_list[i]["name"]} - {inp_list[i]["surname"]} - {inp_list[i]["tel"]} - {inp_list[i]["assignment"]} - {inp_list[i]["wage"]}\n')
+def export_json (database):
+    with open('db.json', 'w', encoding='utf-16') as f:
+        for i in database:
+            f.write(json.dumps(i) + '\n')
+
+def import_json() -> list:
+    items =[]
+    with open('db.json','r', encoding='utf=16') as f:
+        for i in f:
+            items.append(json.loads(i))
+    return items
